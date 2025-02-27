048d752ab7e9c3753d28277031ae729328d22154
Mauro <ma.rg761@gmail.com>
2025-02-27 10:12:01 +0100

    v0.3

--- Password/src/Password.java
+++ Password/src/Password.java
@@ -19,20 +19,41 @@ public class Password {
         StringBuilder pass= new StringBuilder();
 
 
-        System.out.println("Say Size of PASSWORD: ");
-        size= sc.nextInt();
+       
+        
+            try {
+                System.out.println("Say Size of PASSWORD: ");
+                size= sc.nextInt();
 
-            for (int i=0; i<size; i++){
+                boolean flag = false;    
 
-                int random = (int)(Math.random()* letras.length());
-                letra =letras.charAt(random);
-                pass.append(letra) ;        
+                if (size>0){
+                    for (int i=0; i<size; i++){
+
+                        int random = (int)(Math.random()* letras.length());
+                        letra =letras.charAt(random);
+                        pass.append(letra);  
+                        System.out.println("The new PASSWORD is: "+ pass.toString());      
+                    }    
+
+                } else {
+                    flag=true;
+
+                }
+                if (flag){
+                    throw new Exception("Size must be positive");
+                }
+             
                 
+            } catch (Exception e) {
+                System.out.println(e);
+            } 
+       
              
 
-            }
+            
 
-        System.out.println("The new PASSWORD is: "+ pass.toString());
+        
     }
 
 
